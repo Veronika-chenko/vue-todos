d21e4d0e530949bdaa67c41b0e4585feac9c7741
Veronika Kravchenko <aniregma@gmail.com>
2024-08-27 18:24:05 +0300

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <h1>Vue Todos</h1>
+    <p>
+      Track your daily tasks using this todo tracker with abilities to save,
+      complete, edit & delete todos.
+    </p>
+  </main>
+</template>
+
+<style scoped lang="scss">
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+
+  p {
+    text-align: center;
+  }
+}
+</style>
